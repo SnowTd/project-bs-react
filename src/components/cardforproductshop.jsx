4ce1e1a57f5a1c8b9e293a6516da65b06e1302bf
snowtd <akaradechsanpong@gmail.com>
2024-03-18 05:34:36 +0700

    last-removedarkmode

--- src/components/cardforproductshop.jsx
+++ src/components/cardforproductshop.jsx
@@ -1,13 +1,13 @@
 export default function CardProduct ({img , name , price , choose , path}) {
     return (
         <>
-            <div class="w-full bg-whitw border w-72 border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div class="w-full bg-whitw border w-72 border-gray-200 rounded-lg shadow ">
                 <a href="#">
                     <img class="w-auto md:p-8 p-2 rounded-t-lg" src={img} alt="product image" />
                 </a>
                 <div class="md:px-5 md:pb-5">
                     <a href="#">
-                        <h5 class="md:text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{name}</h5>
+                        <h5 class="md:text-xl font-semibold tracking-tight text-gray-900">{name}</h5>
                     </a>
                     <div class="flex items-center md:mt-2.5 md:mb-5">
                         <div class="flex items-center ">
