6a70630ffad5a6f10dce3164bd4be1869a479fda
snowtd <akaradechsanpong@gmail.com>
2024-03-17 17:00:13 +0700

    last-v

--- src/components/jointshop.jsx
+++ src/components/jointshop.jsx
@@ -3,7 +3,7 @@ export default function Joint ({Card, genaral}) {
     return (
         <>
             <div className="container m-24"> 
-                <p className="font-bold text-2xl">PRODUCT JOINT</p>
+                <p className="font-bold text-2xl">GENERAL JOINT</p>
             </div>
             <div className="container mx-auto">
                             <div className="grid grid-cols-4 gap-4">
@@ -16,6 +16,36 @@ export default function Joint ({Card, genaral}) {
             })}
             </div>
                         </div>
+                        <div className="container m-24"> 
+                <p className="font-bold text-2xl">COUNTRY JOINT</p>
+            </div>
+            <div className="container mx-auto">
+                            <div className="grid grid-cols-4 gap-4">
+            {genaral.map((e) => {
+                return (
+                    <>
+                        <Card {...e}/>
+                    </>
+                )
+            })}
+            </div>
+                        </div>
+                        <div className="container m-24"> 
+                <p className="font-bold text-2xl">FESTIVAL JOINT</p>
+            </div>
+            <div className="container mx-auto">
+                            <div className="grid grid-cols-4 gap-4">
+            {genaral.map((e) => {
+                return (
+                    <>
+                        <Card {...e}/>
+                    </>
+                )
+            })}
+            </div>
+                        </div>
+            
         </>
+
     )
 }
